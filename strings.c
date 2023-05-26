cbd0add682af0fd6c6e6684962ea08d9412e2225
RafikSameh <rafiksameh123@gmail.com>
2023-05-26 13:31:22 -0700

    betty rabe3

--- strings.c
+++ strings.c
@@ -21,7 +21,7 @@ int _strcmp(char *s1, char *s2)
 /**
 * _strlen - returns the length of a string
 * @s: is the string to be calculated
-* Return:length of string 
+* Return:length of string
 */
 int _strlen(char *s)
 {
