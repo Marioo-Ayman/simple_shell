deafc040aec6df960da59d1d98c31a7a8368e91c
RafikSameh <rafiksameh123@gmail.com>
2023-05-27 07:18:20 -0700

    final

--- ex.c
+++ ex.c
@@ -1,41 +1,74 @@
 #include "shell.h"
 
 /**
- * execution - function that executes the commands
- * @cp: copy of the command
- * @cmd: the command from the user
- * Return: nothing
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-void execution(char *cp, char **cmd)
+char *_strncpy(char *dest, char *src, int n)
 {
-	pid_t child_pid;
-	int status;
-	char **env = environ;
+	int i, j;
+	char *s = dest;
 
-	child_pid = fork();
-	if (child_pid < 0)
-		perror(cp);
-	if (child_pid == 0)
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		execve(cp, cmd, env);
-		perror(cp);
-		free(cp);
-		free_buffers(cmd);
-		exit(98);
+		dest[i] = src[i];
+		i++;
 	}
-	else
-		wait(&status);
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
-* exit_cmd - to exit shell
-* @command: command from the user
-* @line: from getline
-* Return: nothing
-*/
-void exit_cmd(char **command, char *line)
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-	free(line);
-	free_buffers(command);
-	exit(0);
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
